88dcf56ec2b6e65873cde6e410394b504f4f73ea
Julian-Beck <julianbeck050502@gmail.com>
2024-12-12 11:24:37 +0100

    added d1y24

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,28 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import y2024.day1;
+
 public class Main {
     public static void main(String[] args) {
+        // PART 1
         long startTime = System.nanoTime();
 
-        // MAIN CODE HERE
+        // Define the day
+        long result1 = day1.p1();
 
         long endTime = System.nanoTime();
-        long duration = endTime - startTime;
-        System.out.println("Time: " + (duration / 1_000_000) + "ms");
+        long duration1 = (endTime - startTime) / 1_000_000;
+
+        // PART 2
+        startTime = System.nanoTime();
+
+        // Define the day
+        long result2 = day1.p2();
+
+        endTime = System.nanoTime();
+        long duration2 = (endTime - startTime) / 1_000_000;
+
+        // Print results
+        System.out.format("%15s | %20s\n", "Part 1", "Part 2");
+        System.out.format("%15d | %20d\n", result1, result2);
+        System.out.format("%13dms | %18dms\n", duration1, duration2);
     }
 }
--- src/y2024/day1p1.java
+++
@@ -1,4 +0,0 @@
-package y2024;
-
-public class day1p1 {
-}
